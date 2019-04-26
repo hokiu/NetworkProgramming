483c1544efec1529cb567dcd3f90b21258a38e1c
hokiu <koushiy@icloud.com>
2019-04-26 10:24:00 +0900

    20190426

--- client.c
+++ client.c
@@ -1,5 +1,19 @@
-#include<sys/socket.h>
-#include<arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+void commun(int sock)
+{
+	char buf[256];
+	int len_r;
+	char *message = "ハンムラビ法典";
+	send(sock, message, strlen(message),0);
+	len_r = recv(sock, buf, 256, 0);
+	printf("%s\n,buf");	
+}	
+
 int main(int argc,char**argv){
 	int sock = socket(PF_INET,SOCK_STREAM,0);
 	
@@ -10,7 +24,8 @@ int main(int argc,char**argv){
 	target.sin_port = htons(10001);
 	
 	connect(sock,(struct sockaddr*)&target,sizeof(target));
-	
+	commun(sock);
+
 	
 	close(sock);
 		return 0;
